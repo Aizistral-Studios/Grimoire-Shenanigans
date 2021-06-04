4562c81a3cb6fcbed7276c226a2db5884f960234
Extegral <VictorShadow-Integral@yandex.ru>
2021-06-04 16:14:24 +0200

    Cache clear task fix

--- src/main/java/com/integral/grimoire/tasks/ClearBuildTask.java
+++ src/main/java/com/integral/grimoire/tasks/ClearBuildTask.java
@@ -7,6 +7,7 @@ public class ClearBuildTask extends Delete {
 	public ClearBuildTask() {
 		super();
 		this.delete(this.getProject().file(this.getProject().getBuildDir().getName() + "/classes/main"));
+		this.delete(this.getProject().file(this.getProject().getBuildDir().getName() + "/classes/java/main"));
 	}
 
 }
